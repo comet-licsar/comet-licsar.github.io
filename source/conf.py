a8f19bbb862419dc44cecd2b7e8d857a806a783a
espiritocz <espiritocz@gmail.com>
2022-04-22 13:10:32 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -32,6 +32,7 @@ email = 'cemac-support@leeds.ac.uk'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
