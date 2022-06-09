2470690188d4d4afde784c12084b74abc4c66dba
Richard Rigby <r.rigby@leeds.ac.uk>
2022-06-09 09:38:23 +0100

    Adjust path for Sphinx
    Using '..' should allow importing anything using full path

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../licsar_proc/python/LiCSAR_lib'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # Import datetime for date information:
 import datetime
